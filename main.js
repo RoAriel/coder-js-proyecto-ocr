50b4399ecf87cda14feac72806e9525d75f03fc3
Gonzalez Rodrigo <RoAriel@users.noreply.github.com>
2023-05-23 15:45:01 -0300

    validador de monto ingresado | fix en mostrar historial

--- main.js
+++ main.js
@@ -2,66 +2,67 @@
 
 let user = (prompt('Ingrese usuario:')).trim();
 
-while(user == ''){
+while (user == '') {
 
     alert('Ingrese un usuario valido, por favor.');
     user = (prompt('Ingrese usuario:')).trim();
 }
 
 
-alert('Bienvenido/a '+user);
+alert('Bienvenido/a ' + user);
 
 // Definicion de variables.
-let aportes=0;
-let histDeAportes='';
+let aportes = 0;
+let histDeAportes = '';
 
 // Menu de interacccion
-let opcion= parseInt(prompt(
-    'Sistema de aportes solidarios. Usted quiere:\n'+
-    '1. Ingresar un nuevo aporte.\n'+
-    '2. Ver total de sus aportes.\n'+
-    '3. Ver historial de aportes. \n'+
+let opcion = parseInt(prompt(
+    'Sistema de aportes solidarios. Usted quiere:\n' +
+    '1. Ingresar un nuevo aporte.\n' +
+    '2. Ver total de sus aportes.\n' +
+    '3. Ver historial de aportes. \n' +
     '4. Salir'
 ));
 
 //Funcion que arma el historial de aportes del usuario
-function agregarAlHistorial(monto){
-    let stringMonto= '$'+monto+'\n';
-    histDeAportes+=histDeAportes+stringMonto;
+function agregarAlHistorial(monto) {
+    let stringMonto = '$' + monto + '\n';
+    histDeAportes = histDeAportes + stringMonto;
 }
 
 // Ciclo de inteaccion con el usuario
-while(opcion!=4){
+while (opcion != 4) {
 
-    switch(opcion){
+    switch (opcion) {
 
         //Condicional donde evaluo las opciones del usuario
-        case 1: 
-            
-            monto=parseFloat(prompt('Ingrse el monto a aportar:'));
-            aportes+=monto; // Sumo el nuevo aporte.
-            agregarAlHistorial(monto); // Agrego nuevo monto a la lista de aportes.
-            if(monto>10000){
-                alert('Wow muchas gracias por un aporte tan grande, en serio gracias.');
+        case 1:
+
+            monto = parseFloat(prompt('Ingrse el monto a aportar:'));
+            if(isNaN(monto)){
+                alert('Ingrese valor valido'); // si no es valido el input vuelve al menu raiz
+                break;
             }else{
-            alert('Aporte realizado!.');}
-            break;
+            aportes += monto; // Sumo el nuevo aporte.
+            agregarAlHistorial(monto); // Agrego nuevo monto a la lista de aportes.
+            console.log(histDeAportes);
+            break;}
         case 2:
-            alert('La suma de aportes hasta la fecha es:\n'+'$'+aportes+'.');
+            alert('La suma de aportes hasta la fecha es:\n' + '$' + aportes + '.');
             break;
         case 3:
-            alert('Sus aportes fueron:\n'+histDeAportes);
+            alert('Sus aportes fueron:\n' + histDeAportes);
             break;
         default:
             alert('Opcion no valida');
             break;
     }
-        
-    opcion= parseInt(prompt(
-        'Usted quiere:\n'+
-        '1. Ingresar un nuevo aporte.\n'+
-        '2. Ver total de sus aportes.\n'+
-        '3. Ver historial de aportes. \n'+
+
+    opcion = parseInt(prompt(
+        'Usted quiere:\n' +
+        '1. Ingresar un nuevo aporte.\n' +
+        '2. Ver total de sus aportes.\n' +
+        '3. Ver historial de aportes. \n' +
         '4. Salir'
     ));
 }
