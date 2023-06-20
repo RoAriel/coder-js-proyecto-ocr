c2d4a74b13f78401700d4c27f4eef2ef15ffcd95
rodrigo gonzalez <roariel06@gmail.com>
2023-06-20 15:48:13 -0300

    uso de eventos

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ class Aporte {
     };
 
     getFechaFormateada() {
-        
         return this.fecha.toLocaleDateString();
     };
 };
@@ -21,11 +20,10 @@ class Usuario {
     };
 
     aportar(aporte) {
-        this.aportes.push(new Aporte(this.id,aporte));
+        this.aportes.push(new Aporte(this.id, aporte));
     };
 
     aportesTotales() {
-
         let total = 0;
         for (let aporte of this.aportes) {
 
@@ -36,7 +34,6 @@ class Usuario {
     };
 
     historialDeAportes() {
-
         let histDeAportes = '';
         const aportesOrdenados = this.aportes.sort((a, b) => a > b)
 
@@ -49,22 +46,21 @@ class Usuario {
         return histDeAportes;
     };
 
-    montoDelUtimoAporte(){
-
-        let monto = this.aportes[this.aportes.length-1].monto;
+    montoDelUtimoAporte() {
+        let monto = this.aportes[this.aportes.length - 1].monto;
         return monto;
     };
 
-    fechaDelUtimoAporte(){
-        let fecha = this.aportes[this.aportes.length-1].getFechaFormateada();
+    fechaDelUtimoAporte() {
+        let fecha = this.aportes[this.aportes.length - 1].getFechaFormateada();
         return fecha;
     };
 
 };
 
-function existeUsuarioEnListaDeUsuarios(nombreDeUsuario,usuarios){
+function existeUsuarioEnListaDeUsuarios(nombreDeUsuario, usuarios) {
     let nombresDeLosUsuarios = usuarios.map(usr => usr.nombre.toLowerCase());
-    let nombre =nombreDeUsuario.toLowerCase();
+    let nombre = nombreDeUsuario.toLowerCase();
     return nombresDeLosUsuarios.includes(nombre);
 };
 
@@ -82,13 +78,26 @@ usr4.aportar(700);
 
 const usuarios = [usr1, usr2, usr3, usr4];
 
+// FUNCIONES DE RECORRIDO Y USO DE CLASES
+
+function existeUserDeNombre(usrNombre) {
+    const nombresDeUsuarios = usuarios.map(usr => usr.nombre);
+
+    return (nombresDeUsuarios.includes(usrNombre));
+};
+
+function getUserByName(usrName){
+    return usuarios.find(usr => usr.nombre == usrName);
+};
+
+// USO DEL DOM
 let table = document.getElementById('tablaBody');
 
-function renderizarUsuarios(users){
+function renderizarUsuarios(users) {
 
-    table.innerHTML='';
-    for (const usr of users){
-        table.innerHTML+=`
+    table.innerHTML = '';
+    for (const usr of users) {
+        table.innerHTML += `
         <tr>
              <td>${usr.id}</td>
              <td>${usr.nombre}</td>
@@ -98,5 +107,50 @@ function renderizarUsuarios(users){
         `;
     };
 };
-
 renderizarUsuarios(usuarios);
+
+// Validar Input Usuario.
+let usrValido= false;
+let usrIngresado = document.getElementById('in-user');
+usrIngresado.onkeyup = () => {
+    if (!existeUserDeNombre(usrIngresado.value)) {
+        usrIngresado.style.color = 'red';
+
+    } else {
+        usrIngresado.style.color = 'green';
+        usrValido= true
+    };
+};
+
+// Validar Input Monto.
+let montoValido= false;
+let montoIngresado = document.getElementById('in-monto');
+montoIngresado.onkeyup = () => {
+    if (parseFloat(montoIngresado.value) <= 0) {
+        document.getElementById('bt-aportar').disabled = true;
+    } else {
+        document.getElementById('bt-aportar').disabled = false;
+        montoValido= true;
+    };
+};
+
+// Aportar
+let botonAportar = document.getElementById('bt-aportar');
+botonAportar.addEventListener('click',()=>{
+    if(usrValido && montoValido){
+        getUserByName(usrIngresado.value).aportar(parseFloat(montoIngresado.value));
+        console.log(getUserByName(usrIngresado.value).historialDeAportes())
+        renderizarUsuarios(usuarios);
+
+        // Reset inputs
+        usrIngresado.value = '';
+        montoIngresado.value = '';
+
+        // reset de las variables  XValido
+        usrValido =  false;
+        montoValido =  false;
+
+    }else{
+        alert('Usuario o Monto incorrecto, valide por favor');
+    }
+})
