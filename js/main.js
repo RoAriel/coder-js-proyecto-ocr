4f69d2954b226da724705c8080a032ad97d70635
rodrigo gonzalez <roariel06@gmail.com>
2023-07-13 03:16:10 -0300

    Add Guardar nuevo usuario y simular POST

--- js/main.js
+++ js/main.js
@@ -26,9 +26,6 @@ function updateLSUsers(ususuariosAGuardar) {
     
     let save = JSON.stringify(ususuariosAGuardar);
     localStorage.setItem('usuarios', save);
-    //storageUsers = (localStorage.getItem('usuarios'));
-    //usersParse = JSON.parse(storageUsers);
-    //usuarios = convertirObjLiteralAUsuario(usersParse);
 };
 
 const updateLSTotalRecaudacion = (total) => {
@@ -104,6 +101,7 @@ renderizarUsuarios(usuarios);
 // Buscar Usuarios y Mostrar usuarios
 
 let resultados = document.getElementById('resultadosBody');
+
 function renderizarReusltadosDeBusqueda(users) {
 
     let setUsers = new Set(users);
@@ -279,4 +277,65 @@ console.log(idUserHistValido)
     };
     idInputHist.value = '';
 });
+
+// Agregar nuevo usuario
+
+let maxId = Math.max(...idUsuarios);
+
+const formulario = document.querySelector('form');
+formulario.addEventListener('submit', nuevoUser);
+
+
+function nuevoUser(e){
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target))
+
+    const {nombre,apellido, aporte} = data;
+
+    const newUser = new Usuario(maxId+1, nombre,apellido);
+    newUser.aportar(parseFloat(aporte));
+    usuarios.push(newUser);   
+    let aportes = appAportes()
+    renderizarUsuarios(usuarios);
+    updateLSUsers(usuarios);
+    updateLSTotalRecaudacion(aportes);
+    renderiazarTotal(aportes);
+
+    // UPDATE DE VARIABLES GLOBALES DE LOS ID y NOMBRES
+    idUsuarios = usuarios.map(usr => usr.id);
+    nombresDeUsuarios = usuarios.map(usr => usr.nombre);
+    apellidosDeUsuarios = usuarios.map(usr => usr.apellido);
+
+    // SIMULO LE POST 
+    enviarNuevoUser(newUser);
+    document.querySelector('form').reset()
+    
+
+}
+
+function enviarNuevoUser(user){
+    const URLPOST = 'https://jsonplaceholder.typicode.com/posts';
+    fetch(URLPOST, {
+        method: 'POST',
+        body: JSON.stringify({
+            userId: user.id,
+            title: `${user.nombre}`,
+            body: `${user.historialDeAportes()}`,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(response => response.json())
+        .then((data) =>
+         
+        Toastify({
+            text: `Se agrego el usuario ${data.title} con ID ${data.userId} .`,
+            className: 'tamanioLetra',
+            style: {
+                background: 'linear-gradient(90deg, rgba(26,250,236,1) 0%, rgba(26,250,150,1) 66%)',
+            }
+        }).showToast()
+        )
+}
 
